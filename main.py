f7e61fe703e0292ecf6f03db33c0c4d058acb3eb
Bogdan-pandele <bogpan04@gmail.com>
2025-12-26 16:28:37 +0200

    Phase 1 done

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import socket
 import threading
+import time
 from headers import IPHeaders, TCP_Headers
-from thread_functions import process_packets, capture_packets
+from thread_functions import process_packets, capture_packets, q, stop_signal
 from sys import platform
 
 
-if(platform != "win32" and not platform.startswith("linux")):
+if platform != "win32" and not platform.startswith("linux"):
     raise NotImplementedError(f"{platform} OS not supported")
-
 
 
 t1 = threading.Thread(target=capture_packets, daemon=True)
 t2 = threading.Thread(target=process_packets, daemon=True)
+
 
 t1.start()
 t2.start()
 
 try:
     while True:
-        pass
+        time.sleep(0.1)
 except KeyboardInterrupt:
     print("Stop")
+    stop_signal.set()
+
+    q.join()
--- thread_functions.py
+++ thread_functions.py
@@ -1,47 +1,52 @@
 import queue
 import socket
+import threading
 from headers import IPHeaders, TCP_Headers
 from sys import platform
 
 
-q : queue.Queue[bytes] = queue.Queue()
+q: queue.Queue[bytes] = queue.Queue()
+stop_signal = threading.Event()
+
 
 def capture_packets():
-    if(platform == "win32"):
+    if platform == "win32":
         soc = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
         HOST = "192.168.1.155"
 
         soc.bind((HOST, 0))
         soc.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-    elif(platform == "linux" or platform == "linux2"):
-        soc = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+    elif platform == "linux" or platform == "linux2":
+        soc = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 16 * 1024 * 1024)
+    soc.settimeout(1.0)
 
-    while True:
+    while not stop_signal.is_set():
         try:
             raw_data, _ = soc.recvfrom(65565)
             q.put(raw_data)
+        except socket.timeout:
+            continue
         except Exception as e:
             print(f"Error: {e}")
-
-
 
 
 def process_packets():
     while True:
         raw_data = q.get()
-
+        if platform != "win32":
+            raw_data = raw_data[14:]
         try:
             ip_headers = IPHeaders(raw_data)
 
-            if(ip_headers.protocol == 6): #TCP has value of 6
-                tcp_headers = TCP_Headers(raw_data[ip_headers.ihl * 4:])
+            if ip_headers.protocol == 6:  # TCP has value of 6
+                tcp_headers = TCP_Headers(raw_data[ip_headers.ihl * 4 :])
                 print(ip_headers)
                 print(tcp_headers)
                 print("\n\n")
         except Exception as e:
             print(f"Error: {e}")
-
-        q.task_done()
+        finally:
+            q.task_done()
